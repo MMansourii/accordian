c39f45beaf524a16c762d3460742501bb12fabc0
Mohammad <57011800+Mammad10@users.noreply.github.com>
2021-06-27 15:19:20 +0300

    Updates

---
+++ precache-manifest.0546fdf274cb9fd24bd8f96fb2862286.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9025eb6351b9c726ceafe7d9bcf1d91",
+    "url": "/accordian/index.html"
+  },
+  {
+    "revision": "5fd59f08c910e60445eb",
+    "url": "/accordian/static/css/main.071378ba.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/accordian/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/accordian/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fd59f08c910e60445eb",
+    "url": "/accordian/static/js/main.fd587bc0.chunk.js"
+  },
+  {
+    "revision": "b9012adf2d0eb94649e5",
+    "url": "/accordian/static/js/runtime-main.af1fad76.js"
+  }
+]);
